d2e74d0fc072ee2ba51d522d16c6d79ff19b2dcb
Btvt <bryan.tovantrang@gmail.com>
2018-06-01 11:59:08 +0200

    Added Christmas function in Player to compute the number of new recruits

--- src/Player.java
+++ src/Player.java
@@ -11,6 +11,7 @@ public class Player {
     private Color color;
     private Boolean IsAnIa;
     private ArrayList<Territory> territories = new ArrayList<>();
+    private int past_territories;   // To be updated at the end of a player's turn
 
     /**
      * Default constructor
@@ -73,5 +74,62 @@ public class Player {
 
     public void removeTerritory(Territory territory) {
         this.territories.remove(territory);
+    }
+
+    public int getPast_territories() {
+        return past_territories;
+    }
+
+    public void setPast_territories(int past_territories) {
+        this.past_territories = past_territories;
+    }
+
+    /**
+     * Count the total of potential units earned at the beginning of a player's turn
+     * @return gifts (int)
+     */
+    public int Christmas() {
+        Google tmp_google = new Google();
+        Myfunction mf = new Myfunction();
+
+        int gifts = 0;
+        int factor_one = 0;
+        int factor_two = 0;
+        int factor_three = 0;
+
+        // Factor 1 : Number of owned territories
+        factor_one += Math.floor(this.territories.size() / 3);
+
+        // Factor 2 : Number of owned regions
+        if (this.territories.containsAll(tmp_google.getAsia())) {
+            factor_two += Math.floor(tmp_google.getAsia().size() / 2);
+        }
+        if (this.territories.containsAll(tmp_google.getAfrica())) {
+            factor_two += Math.floor(tmp_google.getAfrica().size() / 2);
+        }
+        if (this.territories.containsAll(tmp_google.getOceania())) {
+            factor_two += Math.floor(tmp_google.getOceania().size() / 2);
+        }
+        if (this.territories.containsAll(tmp_google.getEurope())) {
+            factor_two += Math.floor(tmp_google.getEurope().size() / 2);
+        }
+        if (this.territories.containsAll(tmp_google.getNorth_america())) {
+            factor_two += Math.floor(tmp_google.getNorth_america().size() / 2);
+        }
+        if (this.territories.containsAll(tmp_google.getSouth_america())) {
+            factor_two += Math.floor(tmp_google.getSouth_america().size() / 2);
+        }
+
+        // Factor 3 : Newly conquered territories
+        for (int i=0; i<this.past_territories; i++) {
+            factor_three += mf.random(0,1);
+        }
+
+        // Sum and recruits
+        gifts = factor_one + factor_two + factor_three;
+        if (gifts < 2) {
+            gifts = 2;
+        }
+        return gifts;
     }
 }
