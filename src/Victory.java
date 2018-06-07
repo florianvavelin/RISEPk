0f7d19f471066b91ac66045cf9d8260c8610cf84
marin.mouscadet <marin.mouscadet@ibm.com>
2018-06-08 00:07:01 +0200

    Victory panel fonctionnel

--- src/Victory.java
+++ src/Victory.java
@@ -1,3 +1,7 @@
+import java.awt.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
 public class Victory extends JFrame {
 
     public Victory(String winner) {
@@ -5,17 +9,27 @@ public class Victory extends JFrame {
         Dimension screen = java.awt.Toolkit.getDefaultToolkit().getScreenSize();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
-        this.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setLocation((screen.width - this.getSize().width) / 2, (screen.height - this.getSize().height) / 2);
-        this.setvisible(true);
+        this.setSize(new Dimension(600, 100));
 
-        JPanel victoryPanel = new JPanel();
+        JPanel victoryPanel = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
         this.setContentPane(victoryPanel);
 
-        JLabel winnerName = new JLabel(winner);
-        victoryPanel.add(winnerName);
+        JLabel winnerName = new JLabel(winner + " félicitation tu as gagné !!");
+        c.weightx = 1;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 1;
+        victoryPanel.add(winnerName, c);
 
         JButton recommencer = new JButton("Recommencer");
-        victoryPanel.add(recommencer);
+        c.weightx = 1;
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 1;
+        victoryPanel.add(recommencer, c);
+        validate();
+        this.setVisible(true);
     }
 }
