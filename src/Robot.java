a520831b55539bc96c4c09e241931c7130f57426
Btvt <bryan.tovantrang@gmail.com>
2018-06-08 10:28:44 +0200

    Ro

--- src/Robot.java
+++ src/Robot.java
@@ -1,23 +1,24 @@
 import java.awt.*;
+import java.lang.*;
 
 public class Robot extends Player {
 
-    public Robot(String name, Color color, Boolean IsAnIa) {
-        super(name, color, IsAnIa);
+    public Robot(String name, Color color) {
+        super(name, color);
     }
 
     public  void robot_recruit (int recruits){
         while (recruits > 0) {
-            for (Territory territory : this.getTerritories()) {
-                territory.UncleBenNeedsYou({1, 0, 0});
+            Myfunction mf = new Myfunction();
+            int list_size = this.getTerritories().size();
+            int random_number = mf.random(0, list_size - 1);
+
+            Territory territory = this.getTerritories().get(random_number);
+            territory.UncleBenNeedsYou({1, 0, 0)};
+            recruits--;
             }
-        }
-
     }
 
-    public void robot_attack (){
-
-    }
 
     public void robot_move (){
 
