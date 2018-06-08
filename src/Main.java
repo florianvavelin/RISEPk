aeb45203eb7c5adfc07b22b3fe9bc21eca594541
marin.mouscadet <marin.mouscadet@ibm.com>
2018-06-08 11:21:45 +0200

    cghangement

--- src/Main.java
+++ src/Main.java
@@ -175,8 +175,9 @@ public class Main {
                 else {
                     Robot robot = (Robot) player;
                     robot.robot_recruit(player.Christmas());
-                    robot.robot_attack();
+                    //robot.robot_attack();
                     robot.robot_move();
+                    System.out.println(player.getName() + " : I finished you bitch");
                 }
             }
         }
