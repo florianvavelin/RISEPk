45395f6e6dc6ec2afe3906a4cfdf8ccb6d602d27
Btvt <bryan.tovantrang@gmail.com>
2018-06-08 10:36:36 +0200

    Rob

--- src/Robot.java
+++ src/Robot.java
@@ -3,8 +3,8 @@ import java.lang.*;
 
 public class Robot extends Player {
 
-    public Robot(String name, Color color) {
-        super(name, color);
+    public Robot(String name, Color color, Boolean IsAnIa) {
+        super(name, color, IsAnIa);
     }
 
     public  void robot_recruit (int recruits){
@@ -14,13 +14,34 @@ public class Robot extends Player {
             int random_number = mf.random(0, list_size - 1);
 
             Territory territory = this.getTerritories().get(random_number);
-            territory.UncleBenNeedsYou({1, 0, 0)};
+            int[] babies = {1, 0, 0};
+            territory.UncleBenNeedsYou(babies);
             recruits--;
             }
     }
 
+    public int territory_population (Territory territory) {
+        int population = territory.getArmy_soldiers().size() + territory.getArmy_riders().size() + territory.getArmy_cannons().size();
+        return population;
+    }
 
-    public void robot_move (){
+    public boolean teritory_ennemies (Territory territory) {
+        int counter = 0;
+        for (Territory land : territory.getAdjacents()) {
+            if (land.)
+        }
+    }
+
+    public void robot_attack () {
+        for (Territory territory : this.getTerritories()) {
+            if (territory_population(territory) > 1) {
+
+            }
+        }
+    }
+
+
+    public void robot_move () {
 
     }
 }
